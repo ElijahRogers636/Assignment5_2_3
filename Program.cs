be9d834d79d9bdc186fd8fef453b4c05476e12f5
Elijah Rogers <ejrogersemail@gmail.com>
2024-12-03 11:13:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Assignment5_2_3
+{
+    internal class Program
+    {
+        // Write a program in C# Sharp to print numbers from n to 1 using recursion.
+        static void Main(string[] args)
+        {
+            RecursiveNaturalNumbersDesc(10);
+        }
+
+        // Prints natural numbers from n to 1 in descending order
+        static void RecursiveNaturalNumbersDesc(int natNums)
+        {
+            if (natNums > 0)
+            {
+                Console.WriteLine(natNums);
+                RecursiveNaturalNumbersDesc(natNums - 1);
+
+            }
+        }
+    }
+}
